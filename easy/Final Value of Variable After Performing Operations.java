71877e96d41f59df7216302b2dca0db3a907d2fe
Sumant Warhekar <sumantwarhekar@gmail.com>
2023-02-06 15:47:03 +0530

    Progress on 6th Feb 2023

---
+++ easy/Final Value of Variable After Performing Operations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int answer=0;
+        for(int i=0;i<operations.length;i++){
+            switch(operations[i]){
+                    case"--X":
+                        --answer;
+                        break;
+                    case"X--":
+                        answer--;
+                        break;
+                    case"++X":
+                        ++answer;
+                        break;
+                    case"X++":
+                        answer++;
+                        break;
+            }
+        }
+        return answer;
+    }
+}
