e967a5f062743b38544884b854018600ea29f029
Sumant Warhekar <sumantwarhekar@egmail.com>
2025-02-12 03:01:15 -0500

    started leetcode 75 challenge

---
+++ leetcode 75/merge_strings_alternately_1768.java
@@ -0,0 +1,51 @@
+// 1st Approach: Direct
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+
+        String remainingString="";
+
+        int minlength = Math.min(length1,length2);
+
+        if(minlength==length1){
+            remainingString = word2;
+        }
+
+        if(minlength==length2){
+            remainingString=word1;
+       }
+
+        char chars1[] = word1.toCharArray();
+        char chars2[] = word2.toCharArray();
+
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0;i<minlength;i++){
+            answer.append(chars1[i]);
+            answer.append(chars2[i]);
+        }
+        answer.append(remainingString.substring(minlength));
+        return answer.toString();
+    }
+}
+
+// 2nd Approach: Two Pointer Method
+class Solution2 {
+    public String mergeAlternately(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+
+        StringBuilder answer = new StringBuilder();
+
+        int i=0,j=0;
+
+        while(i<length1 || j<length2){
+            if(i<length1)
+                answer.append(word1.charAt(i++));
+            if(j<length2)
+                answer.append(word2.charAt(j++));
+        }
+        return answer.toString();
+    }
+}
