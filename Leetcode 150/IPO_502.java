f756a643d23fc0da8263c922941f5f1faa1ecc29
Sumant Warhekar <sumantwarhekar@gmail.com>
2026-02-18 21:46:02 -0500

    Started Heap problems

---
+++ Leetcode 150/IPO_502.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        int[][] projects = new int[n][2];
+        for(int i=0;i<n;i++){
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i]; 
+        }
+
+        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        int i=0;
+
+        while(k > 0){
+            while(i < n && projects[i][0] <= w){
+                maxHeap.add(projects[i][1]);
+                i++;
+            }
+            if(maxHeap.isEmpty()){
+                break;
+            }
+            w += maxHeap.poll();
+            k--;
+        }
+        return w;
+    }
+}
